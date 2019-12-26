a8349045372c6f060a4134d3c09dffcc2411a2bf
Kunal Sharma <whatsthereinaname.man@gmail.com>
2019-12-26 13:15:04 +0530

    Update test.java

--- DBMS/DBMS/src/restaurant/test.java
+++ DBMS/DBMS/src/restaurant/test.java
@@ -4,16 +4,16 @@ import java.util.concurrent.Executors;
 public class RaceConditionExample {
 
     public static void main(String[] args) throws InterruptedException {
-        ExecutorService executorService = Executors.newFixedThreadPool(10);
+        ExecutorService executorservice = Executors.newFixedThreadPool(10);
 
         Counter counter = new Counter();
 
         for(int i = 0; i < 1000; i++) {
-            executorService.submit(() -> counter.increment());
+            executorservice.submit(() -> counter.increment());
         }
 
-        executorService.shutdown();
-        executorService.awaitTermination(60, TimeUnit.SECONDS);
+        executorservice.shutdown();
+        executorservice.awaittermination(60, TimeUnit.SECONDS);
     
         System.out.println("Final count is : " + counter.getCount());
     }
